8acb3d9dbf1fbce2270a9001266ca034892e34aa
Elena Levina <qa-levina@yandex.ru>
2022-06-20 01:50:58 +0300

    ДЗ 3.1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1222;
+        int bonusMile = 20;
+        int finalBonus = ticketPrice / bonusMile;
+
+        System.out.println("Ваш бонус " + finalBonus + " миль(я)");
+
+    }
+}
