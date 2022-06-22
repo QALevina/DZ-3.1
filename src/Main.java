d3949065d84ed08922dafec9ba8aff7ebcd4b8ec
Elena Levina <qa-levina@yandex.ru>
2022-06-22 13:35:41 +0300

    Модернизировала приложение

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice = 1222;
+        BonusMilesService service = new BonusMilesService();
+        int price = 1222;
+        int miles = service.calculate(price);
+        System.out.println("Ваш бонус " + miles + " миль(я)");
+
+        /*int ticketPrice = 1222;
         int bonusMile = 20;
         int finalBonus = ticketPrice / bonusMile;
-
-        System.out.println("Ваш бонус " + finalBonus + " миль(я)");
+        System.out.println("Ваш бонус " + finalBonus + " миль(я)");*/
 
     }
 }
