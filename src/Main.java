305529aa8cffe5e4605dd5fcdb7cb85815c80002
Elena Levina <qa-levina@yandex.ru>
2022-06-23 13:18:00 +0300

    Отформатировала код

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,5 @@ public class Main {
         int price = 1222;
         int miles = service.calculate(price);
         System.out.println("Ваш бонус " + miles + " миль(я)");
-
-        /*int ticketPrice = 1222;
-        int bonusMile = 20;
-        int finalBonus = ticketPrice / bonusMile;
-        System.out.println("Ваш бонус " + finalBonus + " миль(я)");*/
-
     }
 }
