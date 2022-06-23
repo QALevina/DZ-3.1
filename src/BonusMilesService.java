a692f79d3ff6512a4a665d658cb92286d798ad23
Elena Levina <qa-levina@yandex.ru>
2022-06-23 17:46:02 +0300

    Поняла, что речь шла об условии из 3 задания)

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int miles;
-        if (cost > 1000) {                  // А где в условии про милли что-либо про 1000?
-            miles = cost / 20;              // Извините, не понимаю, что сделала не так. В этой строке что если переданный параметр больше 1000, то мили расчитываются так
-        } else {                            // во всех остальных случаях
-            miles = 0;                      // мили не начисляются
-        }
+        int miles = cost / 20;
         return miles;
     }
 }
