f8fa20d3bb4c59af0189fcb84026620ccec67e15
Elena Levina <qa-levina@yandex.ru>
2022-06-23 17:34:22 +0300

    Не понятен вопрос(

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
     public int calculate(int cost) {
         int miles;
-        if (cost > 1000) {
-            miles = cost / 20;
-        } else {
-            miles = 0;
+        if (cost > 1000) {                  // А где в условии про милли что-либо про 1000?
+            miles = cost / 20;              // Извините, не понимаю, что сделала не так. В этой строке что если переданный параметр больше 1000, то мили расчитываются так
+        } else {                            // во всех остальных случаях
+            miles = 0;                      // мили не начисляются
         }
         return miles;
     }
